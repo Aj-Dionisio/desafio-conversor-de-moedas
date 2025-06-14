9a0306ef63960b6803030fa656baf56bf2e05966
Antonio <jorge_11000@hotmail.com>
2025-06-14 15:34:16 -0300

    adição de imagem no html e edição no css, andamento no javascript

--- script.js
+++ script.js
@@ -1,87 +1,211 @@
-// // // O QUE A APLICAÇÃO TEM QUE FAZER?
-// // ITENS MACRO 
-// // // A APLICAÇÃO PRECISA:
-// // //1 - RECONHCER QUANDO O BOTÃO É CLICADO 
-// // // 2 - AO SER CLICADO CHAMAR UMA FUNÇÃO
-// // 3 A FUNÇÃO VAI PEGAR O VALOR DO INPUT
-// // 4 FAZER UMA CONTA DE CONVERSÃO 
-// // 5 APRESENTAR O RESULTADO SUBSTITUINDO A VALOR NA MOEDA CONVERTIDA
-
-// // ITENS MICRO:
-// 1 - RECONHCER QUE O BOTÃO FOI CLICADO
-//     1.1 - MAPEAR O BOTÃO - ok
-//     1.2 - CRIE UMA FUNÇÃO QUE IRA GERAR UM CONSOLE.LOG - ok
-//     1.3 - COLOCAR UM addEventListener LIGADO A FUNÇÃO FEITA ANTERIORMENTE - ok
-//     1.4 - PEGAR O VALOR DO INPUT ONDE O VALOR É INSERIDO, ISSO DENTRO DA FUNÇÃO SUBSTITUINDO O CONSOLE.LOG
-//1.4.1 - mapear o input - está mapeado mas não apresenta o valor do input
-
-// 2 - PEGAR O VALOR DO INPUT
-//     2.1 - COLOCAR VARIAVEIS NAS MOEDAS
-//     2.2 - VARIALVEL PARA A CONTA DE CONVERSÃO (VALOR INSERIDO/ VALOR DA MOEDA)
-//     2.3 - APRESENTAR O RESULTADO DA CONTA
-
-
 const convertbutton = document.querySelector('#buttonConvert');
-const currencySelectFrom= document .querySelector ('#currencyFrom');
-const currencySelectTo= document .querySelector ('#currencyTo');
+const currencySelectFrom = document.querySelector('#currencyFrom');
+const currencySelectTo = document.querySelector('#currencyTo');
 const currencyToConvert = document.querySelector('.currencyValueToConvert');
 const currencyTextToConvert = document.querySelector('.currencyTextToConvert');
-const currencyConverted = document.querySelector('.currencyValueConverted')
-const currecyTextConverted = document.querySelector('.currencyTextConverted')
+const currencyConverted = document.querySelector('.currencyValueConverted');
+const currecyTextConverted = document.querySelector('.currencyTextConverted');
+const imageToConvert = document.querySelector('#flagFrom');
+const imageConverted = document.querySelector('#flagTo');
 
 
 function convert() {
     const inputvalue = document.querySelector('.currencyinput').value;
 
-    console.log(currencySelectTo.value);
+    const realToday = 1.00 
+    const dolarToday = 5.60 
+    const euroToday = 6.33 
+    const ieneToday = 0.038
+    const RupiaToday = 0.065
+    const LibraToday = 7.52
+    const DollarCToday = 4.05 
+ 
+    if (currencySelectTo.value === 'dolar') {
+        currencyConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue / dolarToday);
+    }
 
+    if (currencySelectTo.value === 'Euro') {
+        currencyConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: 'currency',
+            currency: 'EUR',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue / euroToday);
+    }
 
+    if (currencySelectTo.value === 'Iene') {
+        currencyConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
+            style: 'currency',
+            currency: 'JPY',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue / ieneToday);
+    }
 
-    const dolarToday = 5.60 // na data de 05/06
-    const euroToday = 6.33 // 09/06
-    const ieneToday = 0.038// 09/06
-    const RupiaToday = 0.065//09/06
-    const LibraToday = 7.52//09/06
-    const FrancobelgaToday = 0.15737// 09/06
-    const DollarCToday = 4.05 //09/06
-    // console.log(currencySelect).value;
-    // const result = inputvalue / dolarToday;
-    // console.log(result);
-//mapeando quando o tipo de moeda é selecionada e ulizando o if para quando ocorrer as atividades
+    if (currencySelectTo.value === 'Rupia') {
+        currencyConverted.innerHTML = new Intl.NumberFormat("en-IN", {
+            style: 'currency',
+            currency: 'INR',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue / RupiaToday);
+    }
 
-if (currencySelectTo.value === 'dolar'){
-    currencyConverted.innerHTML = new Intl.NumberFormat("en-us", {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2,
-    }).format(inputvalue / dolarToday);
+    if (currencySelectTo.value === 'libra') {
+        currencyConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: 'currency',
+            currency: 'GBP',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue / LibraToday);
+    }
+
+    if (currencySelectTo.value === 'Dolar Canadence') {
+        currencyConverted.innerHTML = new Intl.NumberFormat("en-CA", {
+            style: 'currency',
+            currency: 'CAD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue / DollarCToday);
+    }
+
+    if (currencySelectTo.value === 'real') {
+        currencyConverted.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: 'currency',
+            currency: 'BRL',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue / realToday);
+    }
+
+    //  formatando os valores das moedas
+    if (currencySelectFrom.value === 'real') {
+        currencyToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
+            style: 'currency',
+            currency: 'BRL',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue);
+    }
+
+    if (currencySelectFrom.value === 'Dolar Canadence') {
+        currencyToConvert.innerHTML = new Intl.NumberFormat("en-CA", {
+            style: 'currency',
+            currency: 'CAD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue);
+    }
+
+    if (currencySelectFrom.value === 'libra') {
+        currencyToConvert.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: 'currency',
+            currency: 'GBP',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue);
+    }
+
+    if (currencySelectFrom.value === 'dolar') {
+        currencyToConvert.innerHTML = new Intl.NumberFormat("en-US", {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue);
+    }
+
+    if (currencySelectFrom.value === 'Euro') {
+        currencyToConvert.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: 'currency',
+            currency: 'EUR',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue);
+    }
+
+    if (currencySelectFrom.value === 'Iene') {
+        currencyToConvert.innerHTML = new Intl.NumberFormat("ja-JP", {
+            style: 'currency',
+            currency: 'JPY',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue);
+    }
+
+    if (currencySelectFrom.value === 'Rupia') {
+        currencyToConvert.innerHTML = new Intl.NumberFormat("en-IN", {
+            style: 'currency',
+            currency: 'INR',
+            minimumFractionDigits: 0,
+            maximumFractionDigits: 2,
+        }).format(inputvalue);
+    }
+
 }
 
-if (currencySelectTo.value === 'Euro'){
-    currencyConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: 'currency',
-        currency: 'EUR',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2,
-    }).format(inputvalue / euroToday);
+function change() {
+
+    if (currencySelectFrom.value === 'dolar') {
+        currencyTextToConvert.innerHTML = 'Dolar'
+        imageToConvert.src = './assets/Dollar.png'
+    }
+    if (currencySelectFrom.value === 'real') {
+        currencyTextToConvert.innerHTML = 'Real';
+        imageToConvert.src = './assets/Real.png'
+        
+    }
+    if (currencySelectFrom.value === 'Euro') {
+        currencyTextToConvert.innerHTML = 'Euro';
+        imageToConvert.src = './assets/Euro.png'
+         
+    }
+    if (currencySelectFrom.value === 'Iene') {
+        currencyTextToConvert.innerHTML = 'Iene';
+    }
+    if (currencySelectFrom.value === 'Rupia') {
+        currencyTextToConvert.innerHTML = 'Rupia';
+    }
+    if (currencySelectFrom.value === 'libra') {
+        currencyTextToConvert.innerHTML = 'Libra Esterlina';
+    }
+    if (currencySelectFrom.value === 'Dolar Canadence') {
+        currencyTextToConvert.innerHTML = 'Dolar Canadence';
+    }
+
+    if (currencySelectTo.value === 'dolar') {
+        currecyTextConverted.innerHTML = 'Dolar'; 
+    }
+    if (currencySelectTo.value === 'Euro') {
+        currecyTextConverted.innerHTML = 'Euro'; 
+    }
+    if (currencySelectTo.value === 'real') {
+        currecyTextConverted.innerHTML = 'Real'; 
+    }
+    if (currencySelectTo.value === 'Iene') {
+        currecyTextConverted.innerHTML = 'Iene'; 
+    }
+    if (currencySelectTo.value === 'Rupia') {
+        currecyTextConverted.innerHTML = 'Rupia'; 
+    }
+    if (currencySelectTo.value === 'libra') {
+        currecyTextConverted.innerHTML = 'Libra Esterlina'; 
+    }
+    if (currencySelectTo.value === 'Dolar Canadence') {
+        currecyTextConverted.innerHTML = 'Dolar Canadence'; 
+    }
+
+
 }
 
-
-//  formatando os valores das moedas
-    currencyToConvert.innerHTML = new Intl.NumberFormat("pt-br", {
-        style: 'currency',
-        currency: 'BRL',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 2,
-    }).format(inputvalue);
-
-    
-
-
-
-
-}
 
 convertbutton.addEventListener('click', convert);
+
+currencySelectFrom.addEventListener('change', change);
+currencySelectTo.addEventListener('change', change);
 
